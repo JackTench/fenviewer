e98eed991eae9f7295752fe0e07187ea7db99a84
JackTench <79285604+JackTench@users.noreply.github.com>
2024-01-18 18:15:42 +0000

    Comitting this so I remember what not to do.

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,12 @@ struct Board {
     board_h: String,
 }
 
+impl Board {
+    pub fn as_array(&self) -> [String; 8] {
+        [self.board_a, self.board_b, self.board_c, self.board_d, self.board_e, self.board_f, self.board_g, self.board_h]
+    }
+}
+
 fn main() {
     
     // Get CLI argument for given FEN.
@@ -38,5 +44,12 @@ fn main() {
         board_g: String::from(fen[6]),
         board_h: String::from(fen[7]),
     };
+
+    //debug code
+    //println!("{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n", my_board.board_a, my_board.board_b, my_board.board_c, my_board.board_d, my_board.board_e, my_board.board_f, my_board.board_g, my_board.board_h);
+
+    for item in my_board.as_array() {
+        println!("{}", item)
+    }
 
 }
